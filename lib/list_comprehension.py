14ff4f26037db48647e8ea7d88f3c21680464de4
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-11 10:07:23 -0800

    passing tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
+    evens_list = [n for n in num_list if n % 2 == 0]
+    return evens_list if evens_list else []
     pass
 
+
 def make_exclamation(sentence_list):
+    new_list = [s + "!" for s in sentence_list]
+    return new_list
     pass
